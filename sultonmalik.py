5cf7bc16f978daf857e82b1fae6ed6377bb5fdc0
sulton300501 <148205055+sulton300501@users.noreply.github.com>
2023-11-11 16:22:10 +0500

    Update sultonmalik.py

--- sultonmalik.py
+++ sultonmalik.py
@@ -8,6 +8,7 @@ class Aeroport:
         self.aviakompaniyalar.append(aviakompaniya)
 
     
+    
     def chiqar(self):
         print(f"{self.nomi} Aeroportining aviakompaniyalari:")
         for aviakompaniya in self.aviakompaniyalar:
